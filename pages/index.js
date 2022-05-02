841f17387cf1f2b2ab5e3518b383a2551e1ca694
ADRIAN R SOLOMON <70720689+AdrianPrinceSolomon@users.noreply.github.com>
2022-05-02 18:25:18 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -56,13 +56,13 @@ export default function Home() {
                 </p>
               </div>
               <div className="profile__info-group">
-                <h3 className="profile__info-number">15</h3>
+                <h3 className="profile__info-number">3</h3>
                 <p className="profile__info-description">
                   Completed <br /> projects
                 </p>
               </div>
               <div className="profile__info-group">
-                <h3 className="profile__info-number">5</h3>
+                <h3 className="profile__info-number">1</h3>
                 <p className="profile__info-description">
                   Satisfied <br /> customers
                 </p>
